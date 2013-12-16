319a728b4a7a73d5411a933ce97e7a2182f57d33
Koosha Toofan <ktoofan@gmail.com>
2013-12-15 16:30:05 -0800

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoApp::Application.config.secret_key_base = '3a1b2030b40345cfc0b61988ca56fdbfcfa19357ac6e21c6acb3824883e90a736e33887f1f5b9cf1a338a4c927a69f3d35e8d7b0c318fc0bda4af0e7caaf0068'
+DemoApp::Application.config.secret_key_base = '6bb43f44184c1ea385e3aa7f66923f8a165db25e3f3c937035d7636d6f5984b91f4187d5de3023f993a6482d97d938314efae0929dd3d7a90e9a54b8fd4abf6d'
